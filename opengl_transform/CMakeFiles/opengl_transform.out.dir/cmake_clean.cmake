55f8ec75832bfb959ff1d775a6e19beac8a6c402
whdlgp <whdlgp@gmail.com>
2019-12-24 15:51:36 +0900

    Add transform matrix test

---
+++ opengl_transform/CMakeFiles/opengl_transform.out.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opengl_transform.out.dir/Shader.cpp.o"
+  "CMakeFiles/opengl_transform.out.dir/main.cpp.o"
+  "opengl_transform.out.pdb"
+  "opengl_transform.out"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opengl_transform.out.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
